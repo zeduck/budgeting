6dea86a032a6b3e06d8921cac50c1ddb4b830e0f
Roy Zhang <roysmiley.ca@gmail.com>
2024-05-20 20:27:44 -0400

    boi

--- app/api/test/[testId]/route.ts
+++ app/api/test/[testId]/route.ts
@@ -1,5 +1,17 @@
 import { NextRequest, NextResponse } from "next/server"
 
-export const GET = ( request: NextRequest, { params }: { params: { testId: string } }) => {
-  return NextResponse.json({ hello: 'world', testId: params.testId });
+export const GET = ( 
+  request: NextRequest, 
+  { 
+    params 
+  }: { 
+    params: { 
+      testId: string 
+    } 
+  }
+) => {
+  return NextResponse.json({ 
+    hello: 'world', 
+    testId: params.testId,
+  });
 }

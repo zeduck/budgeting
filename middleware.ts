48bd9f39a899851af46c9fda743ad96d140f89e7
Roy Zhang <roysmiley.ca@gmail.com>
2024-05-21 19:09:42 -0400

    hono

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(['/']);
+const isProtectedRoute = createRouteMatcher(['/', '/api(.*)']);
 
 export default clerkMiddleware((auth, request) => {
   if (isProtectedRoute(request)) {

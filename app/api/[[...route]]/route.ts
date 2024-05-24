99dd5c796b0c140f850dbeebcafc0281705517dd
Roy Zhang <roysmiley.ca@gmail.com>
2024-05-24 12:49:44 -0400

    bro

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -7,11 +7,6 @@ export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
 
-app.onError((err, c) => {
-  if (err instanceof HTTPException) return err.getResponse();
-  return c.json({ error: 'Internal server error' }, 500);
-});
-
 const routes = app.route('/accounts', accounts);
 
 export const GET = handle(app);
